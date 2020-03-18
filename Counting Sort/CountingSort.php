f63f30a9347a2c42a5e21cab0eded102bfde3cfb
Brian Evans <ebrian101@gmail.com>
2020-03-18 14:20:43 +0000

    Added counting sort in PHP

---
+++ Counting Sort/CountingSort.php
@@ -0,0 +1,13 @@
+<?php
+class CountingSort{
+    public static function counting_sort($array){
+        $element_count = array_count_values($array);
+        $sorted_array = [];
+        for ($i = 0; $i<count($element_count); $i++){
+            for ($j=0; $j<$element_count[$i]; $j++){
+                array_push($sorted_array, $i);
+            }
+        }
+        return $sorted_array;
+    }
+}
